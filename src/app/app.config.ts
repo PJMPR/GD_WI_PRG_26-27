03a637412aed0ada636342c0b19468940fc83093
PJMPR <adam.urbanow@gmail.com>
2026-02-19 22:20:03 +0100

    logo

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,8 +5,27 @@ import { provideAnimationsAsync } from '@angular/platform-browser/animations/asy
 import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 import { providePrimeNG } from 'primeng/config';
 import Aura from '@primeuix/themes/aura';
+import { definePreset } from '@primeuix/themes';
 
 import { routes } from './app.routes';
+
+const PJATKPreset = definePreset(Aura, {
+  semantic: {
+    primary: {
+      50:  '{red.50}',
+      100: '{red.100}',
+      200: '{red.200}',
+      300: '{red.300}',
+      400: '{red.400}',
+      500: '{red.500}',
+      600: '{red.600}',
+      700: '{red.700}',
+      800: '{red.800}',
+      900: '{red.900}',
+      950: '{red.950}',
+    }
+  }
+});
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -17,7 +36,7 @@ export const appConfig: ApplicationConfig = {
     provideAnimationsAsync(),
     providePrimeNG({
       theme: {
-        preset: Aura,
+        preset: PJATKPreset,
         options: { darkModeSelector: false },
       },
     }),
