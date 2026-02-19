ec964e6c8a3e62f32b258c0148753c8f5b2007c3
PJMPR <adam.urbanow@gmail.com>
2026-02-19 21:24:04 +0100

    menu

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,12 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { DrawerModule } from 'primeng/drawer';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive, DrawerModule, ButtonModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
-  protected readonly title = signal('pj-studies');
-}
+export class App {}
